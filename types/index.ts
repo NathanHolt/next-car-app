637933ca0d19d78c23afe1b0452e3c4fc2741583
NathanHolt <nateofakin@gmail.com>
2023-06-16 23:27:25 -0400

    SearchBar started

--- types/index.ts
+++ types/index.ts
@@ -10,7 +10,7 @@ export interface CustomButtonProps {
 
 export interface CustomManufacturerProps {
     manufacturer: string;
-    setManufacturer: React.Dispatch<React.SetStateAction<string>>;
+    setManufacturer: (manufacturer: string) => void;
 }
 
 export interface CustomFilterProps {
