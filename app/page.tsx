39263a542726e1bc38bce0d0db7145812c4f9736
NathanHolt <nateofakin@gmail.com>
2023-06-18 14:45:58 -0400

    finished CustomFilter and added new type and helper method

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars, testCarData } from '@/utils'
 
 export default async function Home({ searchParams }) {
@@ -31,8 +32,8 @@ export default async function Home({ searchParams }) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -52,7 +52,15 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 }
 
 
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
 
+  searchParams.set(type, value);
+
+  const newPath = `${window.location.pathname}?${searchParams.toString()}`;
+  
+  return newPath;
+}
 
 
 
