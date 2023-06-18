3fce4050e063e3bccb1ddfc3dbdac00d705996ef
NathanHolt <nateofakin@gmail.com>
2023-06-18 14:07:31 -0400

    updated fetchCars, and started CustomFilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 import { CustomFilterProps } from '@/types'
+import { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const CustomFilter = ({ title }: CustomFilterProps) => {
   return (
--- utils/index.ts
+++ utils/index.ts
@@ -2,18 +2,19 @@ import { CarProps, FilterProps } from "@/types";
 
 //comment back in when testing is done
 export async function fetchCars(filters: FilterProps) {
-//     const headers = {
-// 		'X-RapidAPI-Key': 'eb54f0765cmshc7a891205949d3fp17ace4jsn6d7610b2aa8e',
-// 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-// 	}
+  // const { manufacturer, year, model, limit, fuel } = filters;
+  //   const headers = {
+	// 	'X-RapidAPI-Key': 'eb54f0765cmshc7a891205949d3fp17ace4jsn6d7610b2aa8e',
+	// 	'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	// }
 
-//     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
-//         headers: headers,
-//     })
+  //   const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+  //       headers: headers,
+  //   })
 
-//     const result = await response.json();
+  //   const result = await response.json();
 
-//     return result;
+  //   return result;
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
